c4e142aac955613099511d3c76ccf098567726dd
Joaquin <joaquin.f.fernandez@gmail.com>
2020-12-16 13:33:38 -0300

    Fix Windows launcher script.

--- deploy/windows/installer/power-devs.bat
+++ deploy/windows/installer/power-devs.bat
@@ -1,3 +1,3 @@
 powershell.exe -ExecutionPolicy ByPass -File ../scripts/check-vcxsrv.ps1
 cd C:\Users\%USERNAME%\PowerDEVS
-wsl -d Ubuntu-20.04 bash -ic "/opt/powerdevs/bin/run.sh"
+wsl -u powerdevs -d Ubuntu-20.04 bash -ic "/opt/powerdevs/bin/run_win.sh"
