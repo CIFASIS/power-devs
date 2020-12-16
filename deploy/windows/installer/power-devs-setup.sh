64614929116d87e34a061bf1d18eb99ebb9fac5f
Joaquin <joaquin.f.fernandez@gmail.com>
2020-12-16 13:32:12 -0300

    Update setup Windows script.

--- deploy/windows/installer/power-devs-setup.sh
+++ deploy/windows/installer/power-devs-setup.sh
@@ -3,19 +3,22 @@
 sudo apt-get update
 
 echo "Installing PowerDEVS Dependencies"
-sudo apt-get install gnuplot-x11
-sudo apt-get install build-essential
-sudo apt-get install libc6
-sudo apt-get install qt5-default
-sudo apt-get install libqt5svg5
-sudo apt-get install zenity
-sudo apt-get install libhdf5-dev
+sudo apt install -y gnuplot-x11
+sudo apt install -y build-essential
+sudo apt install -y libc6
+sudo apt install -y qt5-default
+sudo apt install -y libqt5svg5
+sudo apt install -y zenity
+sudo apt install -y libhdf5-dev
+
+# Needed to avoid linking error, see: https://github.com/YosysHQ/nextpnr/issues/444#issuecomment-631558250
+sudo strip --remove-section=.note.ABI-tag /usr/lib/x86_64-linux-gnu/libQt5Core.so.5
 
 echo "Installing PowerDEVS"
-sudo dpkg -i -y ./powerdevs.deb
+sudo apt install -y ./scripts/powerdevs.deb
 
 # Packages needed to open user folder browser.
-sudo apt-get install -y konqueror
-sudo apt-get install -y nautilus
+sudo apt install -y konqueror
+sudo apt install -y nautilus
 
 sudo useradd -m -s /bin/bash -G sudo,dialout -p $(openssl passwd powerdevs) powerdevs
