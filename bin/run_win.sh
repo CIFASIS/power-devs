74e2482bbace417185067651297741a30b61eb2a
joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
2021-03-19 12:57:35 -0300

    [iss-9] Squashed commit of the following:
    commit ccc3a16ca656bc72182e8596b89acf25a73db9da
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Mar 19 12:39:41 2021 -0300
    
        Do not enable openGL by default.
    
    commit b40bd6aa1c85c34eb55b7805784981269eef46e6
    Author: joaquin.f.fernandez <joaquin.f.fernandez@gmail.com>
    Date:   Fri Mar 19 12:39:21 2021 -0300
    
        Don't copy scilab files to home folder.

--- bin/run_win.sh
+++ bin/run_win.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+function install_pdevs() {
+   mkdir -p $HOME/powerdevs
+   mkdir -p $HOME/powerdevs/atomics
+   mkdir -p $HOME/powerdevs/bin
+   mkdir -p $HOME/powerdevs/build
+   mkdir -p $HOME/powerdevs/doc
+   mkdir -p $HOME/powerdevs/engine
+   mkdir -p $HOME/powerdevs/examples
+   mkdir -p $HOME/powerdevs/library
+   mkdir -p $HOME/powerdevs/output
+
+   cp /opt/powerdevs/COPYING $HOME/powerdevs/
+   cp /opt/powerdevs/version $HOME/powerdevs/
+   cp -r /opt/powerdevs/atomics/* $HOME/powerdevs/atomics
+   cp -r /opt/powerdevs/bin/* $HOME/powerdevs/bin
+   cp -r /opt/powerdevs/build/* $HOME/powerdevs/build
+   cp -r /opt/powerdevs/doc/* $HOME/powerdevs/doc
+   cp -r /opt/powerdevs/engine/* $HOME/powerdevs/engine
+   cp -r /opt/powerdevs/examples/* $HOME/powerdevs/examples
+   cp -r /opt/powerdevs/library/* $HOME/powerdevs/library
+   cp -r /opt/powerdevs/output/* $HOME/powerdevs/output
+}
+
 if [ ! -d $HOME/powerdevs ]
 then
   # No previous instalation and first run
@@ -44,7 +68,7 @@ then
   fi
 
   echo "Installing PowerDEVS..."
-  cp -a /opt/powerdevs $HOME/powerdevs
+  install_pdevs
   rm $HOME/powerdevs/bin/run.sh
   cd $HOME/powerdevs/bin
   ./pdme
@@ -61,7 +85,7 @@ else
     OW=`zenity --question --text "There is a different version of PowerDEVS on your home folder (ver. $VERHOME). Do you wish to overwrite it with ver. $VEROPT?\nNOTE: you will not lose your models."; echo $?`
     if [ $OW == 0 ];
     then 
-      cp -a -f /opt/powerdevs/* $HOME/powerdevs
+      install_pdevs
       rm $HOME/powerdevs/bin/BackDoor/loader.sce
       rm $HOME/powerdevs/build/lib/*
       rm $HOME/powerdevs/build/objs/*
